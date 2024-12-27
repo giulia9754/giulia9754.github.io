7ba7ad7771c8cb9007a330fa3ab0b4643f0906a4
Giulia <giulia.bangrazi@gmail.com>
2024-12-27 16:52:57 +0100

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,37 @@
+function Footer() {
+    const currentYear = new Date().getFullYear();
+    return (
+        <div>
+            <footer class=" rounded-lg m-4 dark:bg-gray-800">
+                <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                    <span class="text-xs text-gray-500 sm:text-center dark:text-gray-400">
+                        © {currentYear} All Rights Reserved
+                    </span>
+                    {/* <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                        <li>
+                            <a href="#" class="hover:underline me-4 md:me-6">
+                                About
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:underline me-4 md:me-6">
+                                Privacy Policy
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:underline me-4 md:me-6">
+                                Licensing
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:underline">
+                                Contact
+                            </a>
+                        </li>
+                    </ul> */}
+                </div>
+            </footer>
+        </div>
+    );
+}
+export default Footer; //serve a renderla visibile a tutti gli altri componenti
---
+++ src/components/Navbar.js
@@ -0,0 +1,152 @@
+import {
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel
+} from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { FaHome } from 'react-icons/fa';
+import { Link } from 'react-router';
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ');
+}
+
+export default function Navbar() {
+    return (
+        <Disclosure as="nav" className="bg-gray-800">
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                <div className="relative flex h-16 items-center justify-between">
+                    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                        {/* Mobile menu button*/}
+                        <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                            <span className="absolute -inset-0.5" />
+                            <span className="sr-only">Open main menu</span>
+                            <Bars3Icon
+                                aria-hidden="true"
+                                className="block size-6 group-data-[open]:hidden"
+                            />
+                            <XMarkIcon
+                                aria-hidden="true"
+                                className="hidden size-6 group-data-[open]:block"
+                            />
+                        </DisclosureButton>
+                    </div>
+                    <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                        <div className="flex shrink-0 items-center">
+                            <Link
+                                to="/"
+                                className={
+                                    'text-gray-300 hover:bg-gray-700 hover:text-whiterounded-md px-3 py-2 text-sm font-medium'
+                                }
+                            >
+                                <FaHome className="text-white text-2xl" />
+                            </Link>
+                        </div>
+                        <div className="hidden sm:ml-6 sm:block">
+                            <div className="flex space-x-4">
+                                <a
+                                    href="https://flowcv.com/resume/ig1mas518u"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className={classNames(
+                                        'text-gray-300 hover:bg-gray-700 hover:text-whiterounded-md px-3 py-2 text-sm font-medium'
+                                    )}
+                                >
+                                    Resume
+                                </a>
+                                <Link
+                                    to="/progetti"
+                                    className={
+                                        'text-gray-300 hover:bg-gray-700 hover:text-whiterounded-md px-3 py-2 text-sm font-medium'
+                                    }
+                                >
+                                    Projects
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                        <button
+                            type="button"
+                            className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                        >
+                            <span className="absolute -inset-1.5" />
+                            <span className="sr-only">View notifications</span>
+                            <BellIcon aria-hidden="true" className="size-6" />
+                        </button>
+
+                        <Menu as="div" className="relative ml-3">
+                            <div>
+                                <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                    <span className="absolute -inset-1.5" />
+                                    <span className="sr-only">
+                                        Open user menu
+                                    </span>
+                                    <img
+                                        alt=""
+                                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                        className="size-8 rounded-full"
+                                    />
+                                </MenuButton>
+                            </div>
+                            <MenuItems
+                                transition
+                                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                            >
+                                <MenuItem>
+                                    <a
+                                        href="#"
+                                        className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
+                                    >
+                                        Your Profile
+                                    </a>
+                                </MenuItem>
+                                <MenuItem>
+                                    <a
+                                        href="#"
+                                        className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
+                                    >
+                                        Settings
+                                    </a>
+                                </MenuItem>
+                                <MenuItem>
+                                    <a
+                                        href="#"
+                                        className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
+                                    >
+                                        Sign out
+                                    </a>
+                                </MenuItem>
+                            </MenuItems>
+                        </Menu>
+                    </div> */}
+                </div>
+            </div>
+
+            <DisclosurePanel className="sm:hidden">
+                <div className="space-y-1 px-2 pb-3 pt-2">
+                    <DisclosureButton
+                        key="flowcv"
+                        as="a"
+                        target="_blank"
+                        href="https://flowcv.com/resume/ig1mas518u"
+                        className={
+                            'text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium'
+                        }
+                    >
+                        Resume
+                    </DisclosureButton>
+                    <DisclosureButton
+                        key="flowcv"
+                        as="a"
+                        className={
+                            'text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium'
+                        }
+                    >
+                        <Link to="/progetti">Projects</Link>
+                    </DisclosureButton>
+                </div>
+            </DisclosurePanel>
+        </Disclosure>
+    );
+}
